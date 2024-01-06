62fc987ae17115a5d0b3e5bb36b4f9ac3b7b78e0
subhash131 <subhashnayak131@gmail.com>
2024-01-06 22:42:18 +0530

    import sec 3 to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Section2 from "@/components/Section2";
+import Section3 from "@/components/Section3";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Section2 />
+      <Section3 />
     </main>
   );
 }
