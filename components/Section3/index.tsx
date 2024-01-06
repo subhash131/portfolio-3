dbc3f4d1305526bb252376ef838d58eb9e2c6c38
subhash131 <subhashnayak131@gmail.com>
2024-01-06 22:41:56 +0530

    section 3 basic setup

--- components/Section3/index.tsx
+++ components/Section3/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./section3.module.css";
 
 const Section3 = () => {
-  return <div>Section3</div>;
+  return <div className={styles.container}>Section3</div>;
 };
 
 export default Section3;
