82e9d9d5c1b4af3842453263815ac13e85bca4fb
subhash131 <subhashnayak131@gmail.com>
2024-01-07 23:16:01 +0530

    grid optimization

--- components/Section2/index.tsx
+++ components/Section2/index.tsx
@@ -5,16 +5,6 @@ import CompaniesGrid from "./Grid";
 const Section2 = () => {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.title_container}>
-        <h1 className={styles.title}>
-          From pioneering innovative software solutions to optimizing digital
-          experiences.
-        </h1>
-        <p className={styles.title_description}>
-          my journey as a software engineer has spanned impactful collaborations
-          with industry leaders such as
-        </p>
-      </div> */}
       <div className={styles.grid_container}>
         <CompaniesGrid />
       </div>
